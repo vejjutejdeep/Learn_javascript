87af6a65cbb92671f447f4aee0fb911907da8329
vejjutejdeep <tej2544@msitprogram.net>
2023-11-30 09:22:39 +0530

    change in script

--- conditionalStatments/script.js
+++ conditionalStatments/script.js
@@ -1,17 +1,21 @@
 let age = 17
 
-let knowDriving = true
+let knowDriving = false
 
 if (age > 17 && knowDriving){
 
     console.log(`Can be issued with driving lisence since he knows driving and of leagal age.`)
+    alert(`Can be issued with driving lisence since he knows driving and of leagal age.`)
 }
 else if(age > 17 && !knowDriving){
     console.log(`He get the lisence after he learns driving`)
+    alert(`He get the lisence after he learns driving`)
 }
 else if(age <= 17 && knowDriving){
     console.log(`He get the lisence after reaching legal age`)
+    alert(`He get the lisence after reaching legal age`)
 }
 else{
     console.log(`He can't get a driving lisence.`)
+    alert(`He can't get a driving lisence.`)
 }
